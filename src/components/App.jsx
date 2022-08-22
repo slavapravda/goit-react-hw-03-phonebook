f4a208c3bc3864f45aa614f4ef921db7a9e15244
Slava Pravda <pravdaslava7@gmail.com>
2022-08-22 14:00:19 +0300

    add save in local storage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,6 +17,23 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'));
+
+    if (contacts?.length) {
+      this.setState({
+        contacts: contacts,
+      });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+
+    if (prevState.contacts !== contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
+
   formSubmitHandler = data => {
     const { contacts } = this.state;
 
